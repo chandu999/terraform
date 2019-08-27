158320e55888617f6f8cc05c4bb304b74f945efc
chandu999 <chandu.paleti9@gmail.com>
2019-08-27 04:17:00 +0000

    first commit

---
+++ instance.tf
@@ -0,0 +1,11 @@
+"{
+  access_key = "AKIAQ7JNG4PSJ43MWHGF"
+  secret_key = "W+QiMS3Mkm8nQY9uF+7NqQS2o7kJ9QWCpxqD45Nk"
+  region     = "us-east-2b"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0050ea63"
+  instance_type = "t2.micro"
+
+}
